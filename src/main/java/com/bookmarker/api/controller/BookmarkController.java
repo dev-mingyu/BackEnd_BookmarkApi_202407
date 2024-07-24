b30c9e433396e0168da6c29a4ae9f03b6b14e960
mgpark <dev.mgpark@gmail.com>
2024-07-24 17:55:39 +0900

    cors 수정

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -7,6 +7,7 @@ import com.bookmarker.api.service.BookmarkService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +18,8 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public BookmarksDTO<?> getBookmarks(@RequestParam(required = false) String query, Pageable pageable) {
+    public BookmarksDTO<?> getBookmarks(@RequestParam(required = false) String query,
+                                        @PageableDefault(size = 10) Pageable pageable) {
 
         if(query == null || query.isBlank()) {
             return bookmarkService.getBookmarks(pageable);
